27d4dbc85bc1eb4cb2e8521df2bcf3f0a9179a0f
刘华佼 <liuhuajiao@liuhuajiaodeMacBook-Air.local>
2017-06-08 11:37:42 +0800

    change sendcloud to mail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603065956) do
+ActiveRecord::Schema.define(version: 20170608025558) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -85,6 +85,10 @@ ActiveRecord::Schema.define(version: 20170603065956) do
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false
     t.string   "name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
